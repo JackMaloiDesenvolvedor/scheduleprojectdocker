4893cdc3e218eb82bd8adae741b208b79dbe0d5c
JackMaloiDesenvolvedor <jackmaloidesenvolvedor@gmail.com>
2024-04-01 22:11:01 -0300

    In progress

--- scheduleproject/contact/views/contact_forms.py
+++ scheduleproject/contact/views/contact_forms.py
@@ -1,36 +1,8 @@
 from django.core.paginator import Paginator
+from contact.forms import ContactForm
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
-from django import forms
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-        )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
-
 
 def create(request):
     if request.method == 'POST':
